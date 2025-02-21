96930ad4d3c29d354f18b0c06e39b2ebceb38203
Dylan Ray <ddray@email.unc.edu>
2025-02-21 12:58:19 -0500

    stuff

--- src/simulations/simulate_slim.py
+++ src/simulations/simulate_slim.py
@@ -4,15 +4,18 @@ import argparse
 import logging
 
 import sys
-sys.path.append('popgenml/data')
+sys.path.append(os.getcwd())
 
-from simulators import SlimSimulator
-from io_ import append_to_ms
+from popgenml.data.simulators import SlimSimulator
+from popgenml.data.io_ import append_to_ms
 import numpy as np
 
 def array_to_binary_string(arr):
     binary_string = ''.join(str(num) for num in arr)
     return binary_string + '\n'
+
+from mpi4py import MPI
+
 
 # use this format to tell the parsers
 # where to insert certain parts of the script
@@ -25,7 +28,7 @@ def parse_args():
     parser.add_argument("--verbose", action = "store_true", help = "display messages")
     parser.add_argument("--ifile", default = "src/simulations/slim/introg_bidirectional.slim")
     parser.add_argument("--L", default = 10000, type = int)
-    parser.add_argument("--n_replicates", default = 10, type = int)
+    parser.add_argument("--n_replicates", default = 1, type = int)
     
     parser.add_argument("--args", default = "-d sampleSizePerSubpop={} -d donorPop={} -d st={} -d mt={} -d mp={} -d introS={}")
     parser.add_argument("--vals", default = "64,1,4,0.25,1,0")
@@ -43,11 +46,13 @@ def parse_args():
         if not os.path.exists(args.odir):
             os.system('mkdir -p {}'.format(args.odir))
             logging.debug('root: made output directory {0}'.format(args.odir))
-    # ${odir_del_block}
 
     return args
 
 def main():
+    # configure MPI
+    comm = MPI.COMM_WORLD
+    
     args = parse_args()
     
     args_ = args.args
@@ -56,24 +61,18 @@ def main():
     
     sim = SlimSimulator(args.ifile, args_)
     
-    ms_ofile = open(os.path.join(args.odir, 'mig.msOut'), 'w')
-    anc_ofile = open(os.path.join(args.odir, 'out.anc'), 'w')
+    #ms_ofile = open(os.path.join(args.odir, 'mig.msOut'), 'w')
+    #anc_ofile = open(os.path.join(args.odir, 'out.anc'), 'w')
     
-    for ij in range(args.n_replicates):
+    for ij in range(comm.rank, args.n_replicates, comm.size):
         X, pos, y = sim.simulate(*args.vals.split(','))
         
         y = y.T.astype(np.uint8)
         
-        append_to_ms(ms_ofile, X, pos)
-        for y_ in y:
-            line = array_to_binary_string(y_)
-            anc_ofile.write(line)
-        
-    anc_ofile.close()
-    ms_ofile.close()
-    
-    os.system('gzip {} && gzip {}'.format(os.path.join(args.odir, 'mig.msOut'), os.path.join(args.odir, 'out.anc')))
-    
+        if ij % 50 == 0:
+            logging.info('on replicate {}...'.format(ij))
+        np.savez_compressed(os.path.join(args.odir, '{0:05d}.npz'.format(ij)), x = X, y = y)
+
     # ${code_blocks}
 
 if __name__ == '__main__':
