67f0ddc29ee156392116f9391699e60fb12add3f
Dylan Ray <ddray@email.unc.edu>
2025-03-17 19:30:46 -0400

    fixed import

--- popgenml/models/gcn_layers.py
+++ popgenml/models/gcn_layers.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import copy
 
-from layers import MLP
+from popgenml.models.layers import MLP
 from torch import autograd
 from typing import Callable, Any, Optional, Tuple, List
 
