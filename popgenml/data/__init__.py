785d3f434cb464d3086931f78e82ea384f29d7e3
kilmoretrout <ddray@email.unc.edu>
2026-09-09 21:06:19 -0400

    stuff

--- popgenml/data/__init__.py
+++ popgenml/data/__init__.py
@@ -3,4 +3,4 @@ from .relate import relate
 from .singer import singer
 from .trees import PGTreeSequence
 
-from .io_ import load_vcf, load_ms, append_to_ms, write_to_ms
+from .io_ import load_vcf, load_ms, append_to_ms, write_to_ms, write_vcf
